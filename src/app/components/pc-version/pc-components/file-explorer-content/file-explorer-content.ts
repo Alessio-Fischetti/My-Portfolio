8ae74c875c096e40ff33db99a6eec45280a117b7
Alessio Fischetti <afischetti.work@gmail.com>
2026-02-18 14:29:54 +0300

    WIP file_explorer

--- src/app/components/pc-version/pc-components/file-explorer-content/file-explorer-content.ts
+++ src/app/components/pc-version/pc-components/file-explorer-content/file-explorer-content.ts
@@ -1,11 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-file-explorer-content',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './file-explorer-content.html',
   styleUrl: './file-explorer-content.scss'
 })
 export class FileExplorerContent {
+  /* Variables */
+  rotateListArrow: boolean = false;
 
 }
