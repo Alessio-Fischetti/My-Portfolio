711ac6d0222f65ce310e2659e88ef778efe4b8d5
Alessio Fischetti <afischetti.work@gmail.com>
2026-02-21 16:24:34 +0300

    creato e gestıto contattamı

--- src/app/components/pc-version/pc-components/modal-content/contact-content/contact-content.ts
+++ src/app/components/pc-version/pc-components/modal-content/contact-content/contact-content.ts
@@ -1,8 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { ModalSevice } from '../../../../../service/modal-service';
-
 
 @Component({
   selector: 'app-contact-content',
@@ -12,15 +9,40 @@ import { ModalSevice } from '../../../../../service/modal-service';
 })
 export class ContactContent {
   /* Variables */
-  componentSub!: Subscription;
-  fileToOpen!: string;
+  contacts: { contactType: string, contactValue: string, icon: string, copy: boolean }[] = [
+    {
+      contactType: 'Numero Telefonico', contactValue: '+39 380 194 9521', icon: 'assets/icons/telephone.png', copy: true
+    },
+    {
+      contactType: 'Email', contactValue: 'afischetti.work@gmail.com', icon: 'assets/icons/email_container.png', copy: true
+    },
+    {
+      contactType: 'Posizione', contactValue: 'Bologna, Italia, IT', icon: 'assets/icons/location.png', copy: false
+    },
+  ]
+  links: { linkType: string, linkValue: string, icon: string, actualLink: string }[] = [
+    {
+      linkType: 'Linkedin', linkValue: 'linkedin.com/alessio-fischetti', icon: 'assets/icons/linkedin.svg', actualLink: 'https://www.linkedin.com/in/alessio-fischetti-a085ab20a/'
+    },
+    {
+      linkType: 'GitHub', linkValue: 'github.com/Alessio-Fischetti', icon: 'assets/icons/github.svg', actualLink: 'https://github.com/Alessio-Fischetti'
+    },
+    {
+      linkType: 'Portfolio', linkValue: 'lessio-fischetti.github.io/Software-Developer-Portfolio', icon: 'assets/icons/online.png', actualLink: 'https://alessio-fischetti.github.io/Software-Developer-Portfolio'
+    },
+    {
+      linkType: 'Medium', linkValue: 'medium.com/@afischetti.work', icon: 'assets/icons/medium.svg', actualLink: 'https://medium.com/@afischetti.work'
+    },
 
-  constructor(private modalService: ModalSevice) { }
+  ]
 
-  ngOnInit() {
-  }
-
-  ngOnDestroy() {
-    this.componentSub.unsubscribe();
+  /* Copia il valore */
+  copy(value: string) {
+    if (value === '+39 380 194 9521') {
+      const cleanNumber = value.replace(/\s+/g, '');
+      navigator.clipboard.writeText(cleanNumber);
+    } else {
+      navigator.clipboard.writeText(value)
+    }
   }
 }
